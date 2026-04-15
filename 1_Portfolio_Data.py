b4f8bde8a92a7f3b2899a60a82d45c67d24b2d4d
ben rose <bros@2030districts.org>
2026-04-15 09:51:58 -0500

    years added!

--- 1_Portfolio_Data.py
+++ 1_Portfolio_Data.py
@@ -604,7 +604,7 @@ fig_eui_bar = px.bar(
     title='Average Site EUI by Data Year (Bar Chart)',
     labels={'eui': 'EUI (kBtu/ft^2)', 'datayear': 'Data Year', 'series': ''},
     category_orders={'series': ['Baseline EUI', 'Actual EUI', 'Target EUI']},
-    text='eui',
+    text='avg_siteeui',
     color_discrete_map={
         'Actual EUI': '#F7C900',
         'Baseline EUI': '#878888',
